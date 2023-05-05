809a86d5164f038bbb80260fc70f52cdee71cb12
anton <anton.dontsovgs@mail.ru>
2023-05-05 15:02:40 +0300

    Forms associated with models

--- coolsite/women/views.py
+++ coolsite/women/views.py
@@ -24,13 +24,10 @@ def about(request):
 
 def addpage(request):
     if request.method == 'POST':
-        form = AddPostForm(request.POST)
+        form = AddPostForm(request.POST, request.FILES)
         if form.is_valid():
-            try:
-                Women.objects.create(**form.cleaned_data)
-                return redirect('home')
-            except:
-                form.add_error(None, 'Ошибка добавления поста')
+            form.save()
+            return redirect('home')
     else:
         form = AddPostForm()
     return render(request, 'women/addpage.html', {'form':form, 'menu': menu, 'title': 'Добавление статьи'})
